6a86cba47467942001658df813e4e4435a3cb589
KoketjoSethobja <sethobjakoketjo90@gmail.com>
2022-05-12 16:53:40 +0200

    server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', '*'],
+    origin: ['http://localhost:3000', 'https://koketjocomgallery.netlify.app/', '*'],
     method: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
 }));
